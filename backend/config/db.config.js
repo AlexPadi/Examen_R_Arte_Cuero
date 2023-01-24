7974f366816a25e0a6143584df5cf7a777c639c6
Dio-Enviroment <dio.german93@gmail.com>
2023-01-24 01:54:46 -0500

    mongo

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://localhost:27017/asu"
+    url: "mongodb://localhost/asu"
   };
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:4200"
 };
 
 app.use(cors(corsOptions));
@@ -15,14 +15,8 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
 const db = require("./models");
+
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -35,6 +29,17 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
+
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./routes/student.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);

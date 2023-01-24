b2c928c70c8b2b026ad574096d0db102d3957429
Dio-Enviroment <dio.german93@gmail.com>
2023-01-23 23:44:56 -0500

    asi

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const URI='mongodb://localhost/gastos';
+const URI='mongodb://localhost/asu';
 mongoose.connect(URI)
 .then(db=> console.log('BD conectada'))
 .catch(err => console.error(err));
